81e01c66d71e048314b2d7ac57a18b61601a5612
Jordan Shaw <jrdnshw@gmail.com>
2015-11-28 15:07:41 -0500

    added sonar code and neopixel libs together

--- cc-antisocial.ino
+++ cc-antisocial.ino
@@ -1,7 +1,33 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
 #include <Ultrasonic.h>
 #include <Array.h>
 
+
+// NEOPIXEL init
+// ===========================
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+#define PIN 11
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);
+
+
 // Init an Ultrasonic object
+// ===========================
 // TODO: Put these inits and declarations into an array
 Ultrasonic ultrasonicOne(3, 2);
 Ultrasonic ultrasonicTwo(5, 4);
@@ -30,8 +56,8 @@ int rawArray[size] = {1,2,3};
 int sensorArrayValue[size];
 Array<int> array = Array<int>(sensorArrayValue, size);
 
-
-
+// Timer init
+// ===========================
 const long interval = 500;
 unsigned long previousMillis = 0;
 
@@ -44,26 +70,42 @@ void setup() {
   //initialize serial communications at a 9600 baud rate
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT); // Set pin as OUTPUT
-  // send a byte to establish contact until receiver responds 
-//  establishContact();
 
- for (byte i=0; i<array.size(); i++){
-//   Serial.print(array);
-//   Serial.print(", ");
- }
- Serial.println("\nSpecial functionality:");
- Serial.print("\tMinimum value:");
- Serial.print(array.getMin());
- Serial.println();
- Serial.print("\tMaximum value:");
- Serial.print(array.getMax());
- Serial.println();
- Serial.print("\tAverage value:");
- Serial.print(array.getAverage());
- Serial.println();
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  
+  // send a byte to establish contact until receiver responds 
+  //  establishContact();
+
+   for (byte i=0; i<array.size(); i++){
+  //   Serial.print(array);
+  //   Serial.print(", ");
+   }
+   Serial.println("\nSpecial functionality:");
+   Serial.print("\tMinimum value:");
+   Serial.print(array.getMin());
+   Serial.println();
+   Serial.print("\tMaximum value:");
+   Serial.print(array.getMax());
+   Serial.println();
+   Serial.print("\tAverage value:");
+   Serial.print(array.getAverage());
+   Serial.println();
+   
 }
 
 void loop() {
+
+  // Some example procedures showing how to display to the pixels:
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip.Color(0, 255, 0), 50); // Green
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
 
   unsigned long currentMillis = millis();
 
@@ -176,3 +218,90 @@ void establishContact() {
   }
 }
 
+
+// NEOPIXEL Functions
+// ========================
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
